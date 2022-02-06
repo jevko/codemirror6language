3e2f061070502544c37057b001275363bb641bc8
Dariusz Jędrzejczak <dariusz.jedrzejczak.work+github@gmail.com>
2022-02-06 22:40:02 +0100

    Adapted template to support Jevko in CodeMirror 6

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,27 @@ import {parser} from "./syntax.grammar"
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
 import {styleTags, tags as t} from "@codemirror/highlight"
 
-export const EXAMPLELanguage = LRLanguage.define({
+export const jevkoLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
+        Jevko: delimitedIndent({closing: "]"})
       }),
       foldNodeProp.add({
-        Application: foldInside
+        Subjevko: foldInside,
       }),
       styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren
+        'Jevko/Text': t.string,
+        'Subjevko/Text': t.labelName,
+        "[ ]": t.paren
       })
     ]
   }),
   languageData: {
-    commentTokens: {line: ";"}
+    // commentTokens: {line: ";"}
   }
 })
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export function jevko() {
+  return new LanguageSupport(jevkoLanguage)
 }
